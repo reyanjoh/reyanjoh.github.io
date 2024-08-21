e163da86726436a74baee1d55780fb3305359926
Reyan <reyanjohnnarvaja@gmail.com>
2024-08-22 03:25:05 +0800

    next-init

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/nextjs-github-pages",
-    images: {
-        unoptimized: true,
-    },
 };
 
 export default nextConfig;
