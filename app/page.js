0dcd90b8d698ee78504a0cb5df119f9cae716094
Reyan <reyanjohnnarvaja@gmail.com>
2024-08-22 03:28:58 +0800

    next-init

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
+              src="./vercel.svg"
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
@@ -31,7 +31,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="./next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
