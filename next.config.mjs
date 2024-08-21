8ab1063ebf6335ce794d2110cfa42e8afbe84631
Reyan <reyanjohnnarvaja@gmail.com>
2024-08-22 04:48:40 +0800

    export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: "/reyanjoh.github.io",
+    images: {
+        unoptimized: true,
+      },
 };
 
 export default nextConfig;
