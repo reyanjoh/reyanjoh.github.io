65123d688df61d65331e7f2f8f9e961fcdb4c78d
Reyan <reyanjohnnarvaja@gmail.com>
2024-08-22 05:24:05 +0800

    export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: "export",
+    basePath: "/reyanjoh.github.io",
+    images: {
+        unoptimized: true,
+      },
+}
 
 module.exports = nextConfig
